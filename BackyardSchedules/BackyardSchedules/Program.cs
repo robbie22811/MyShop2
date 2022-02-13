fb13d6f69582c7311cdaff08c4c5c712c9df01d4
robbie22811 <98604431+robbie22811@users.noreply.github.com>
2022-02-13 10:09:46 -0600

    New logic

--- BackyardSchedules/BackyardSchedules/Program.cs
+++ BackyardSchedules/BackyardSchedules/Program.cs
@@ -8,19 +8,17 @@ namespace BackyardSchedules
 {
     class Program
     {
-        private const int BYE = -1;
-        
         static void Main(string[] args)
         {
             int byeID = -1;
 
             // Get the events
             // Textbox on webform -- enter game names
-            List<string> eventList = new List<string> { "bags", "darts", "bocce", "rollors" };
+            List<string> eventList = new List<string> { "bags", "darts" };
 
             // Get the teams.
             // Textbox on the webform -- string all_teams = txtTeams.Text;
-            string all_teams = "One\rTwo\nThree\nFour\nFive\nSix\nSeven\nEight\nNine";
+            string all_teams = "One\rTwo\nThree\nFour";
             List<string> team_names = GenerateTeams(all_teams);
 
             if (team_names.Count % 2 > 0)
@@ -28,54 +26,34 @@ namespace BackyardSchedules
                 team_names.Add("BYE");
                 byeID = team_names.Count - 1;
             }
-                
+
 
             // Get the schedule.
             int num_teams = team_names.Count;
-            Console.WriteLine(SetTournament(team_names, eventList, byeID));
+            int[,] gamesToPlay = GenerateGames(num_teams);
 
+            int numofRounds = num_teams - 1;
 
-            //int[,] schedule = GenerateRoundRobin(num_teams);
+            int[,] eventCounter = new int[num_teams, eventList.Count];
+            int[,] roundEvents = new int[numofRounds, eventList.Count];
+            int[,] roundTeams = new int[numofRounds, num_teams];
 
-            //int gamesPerRound = (int)Math.Floor((double)num_teams / 2);
-            //int roundsToPlay = schedule.GetUpperBound(1);
-            //int maxAttempts = gamesPerRound * roundsToPlay;
+            string results = SetTournament(gamesToPlay, team_names, eventList, eventCounter, roundEvents, roundTeams, byeID);
 
-            //int[,] eventCounter = new int[num_teams, eventList.Count];
-            //int[,] roundCounter = new int[roundsToPlay + 1, num_teams];
-            //for (int round = 0; round <= roundsToPlay; round++)
-            //{
-            //    for (int game = 0; game < gamesPerRound; game++)
-            //    {
-            //        string gameToPlay = SetGame(round, game, eventList, team_names, schedule, eventCounter, roundCounter, maxAttempts);
-            //        Console.WriteLine($"Round {round + 1}, Game {game + 1} = {gameToPlay}");
-            //    }
-            //}
+            for (
+                int i = 0; i <= gamesToPlay.GetUpperBound(0); i++)
+            {
+                for (int ii = 0; ii <= gamesToPlay.GetUpperBound(1); ii++)
+                    if (gamesToPlay[i,ii] == 0)
+                        Console.WriteLine($"{team_names[i]} vs. {team_names[ii]}");
+            }
 
-            //string txt = "";
-            //for (int round = 0; round <= schedule.GetUpperBound(1); round++)
-            //{
-            //    txt += "Round " + (round + 1) + ":\r\n";
-            //    for (int team = 0; team < num_teams; team++)
-            //    {
-            //        if (schedule[team, round] == BYE)
-            //        {
-            //            txt += "    " + team_names[team] + " (bye)\r\n";
-            //        }
-            //        else if (team < schedule[team, round])
-            //        {
-            //            txt += "    " + team_names[team] + " v " +
-            //                team_names[schedule[team, round]] + "\r\n";
-            //        }
-            //    }
-            //}
-
-            //Console.WriteLine(txt);
+            Console.WriteLine(results);
             Console.ReadLine();
 
         }
 
-        static string SetTournament(List<string> teamNames, List<string> games, int byeID)
+        static string SetTournament(int[,] gamesToPlay, List<string> teamNames, List<string> games, int[,] eventCounter, int[,] roundCounter, int[,] teamRoundCounter, int byeID)
         {
             string roundPair = "";
 
@@ -84,39 +62,67 @@ namespace BackyardSchedules
 
             int teamId;
             int team2Id;
-            
-            int[,] eventCounter = new int[n, games.Count];
-            int[,] roundCounter = new int[numofRounds, games.Count];
 
-            for (var r = 1; r < n; r++)
+            for (int r = 0; r < numofRounds; r++)
             {
-                roundPair += "\n" + formatter(r) + ":";
-                for (var i = 1; i <= n/2; i++)
+                for (int i = 0; i <= gamesToPlay.GetUpperBound(0); i++)
                 {
-                    if (i==1)
+                    for (int ii = 0; ii <= gamesToPlay.GetUpperBound(1); ii++)
                     {
-                        teamId = 0;
-                        team2Id = (n - 1 + r - 1) % (n - 1) + 1;
-                        //roundPair += " [" + formatter(1) + "," + formatter((n - 1 + r - 1) % (n - 1) + 2) + "]";
+                        if (gamesToPlay[i, ii] == 0)
+                        {
+                            if (teamRoundCounter[r, i] == 0 && teamRoundCounter[r, ii] == 0)
+                            {
+                                for (int gg = 0; gg < games.Count; gg++)
+                                {
+                                    if (eventCounter[i, gg] == 0 && eventCounter[ii, gg] == 0 && roundCounter[r, gg] == 0)
+                                    {
+                                        roundPair += "\n" + r + ": " + teamNames[i] + " will play " + teamNames[ii] + " in " + games[gg];
+                                        eventCounter[i, gg] = 1;
+                                        eventCounter[ii, gg] = 1;
+                                        gamesToPlay[i, ii] = r + 1;
+                                        roundCounter[r, gg] = 1;
+                                        teamRoundCounter[r, i] = 1;
+                                        teamRoundCounter[r, ii] = 1;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
                     }
-                    else
-                    {
-                        teamId = (r + i - 2) % (n - 1) + 1;
-                        team2Id = (n - 1 + r - i) % (n - 1) + 1;
-                        //roundPair += " [" + formatter( (r+i-2) % (n-1) + 2) + "," + formatter((n - 1 + r - i) % (n - 1) + 2) + "]";
-                    }
-                    if (byeID >= 0 && (teamId == byeID || team2Id == byeID))
-                    {
-                        roundPair += " BYE  [" + teamNames[teamId] + "," + teamNames[team2Id] + "]";
-                    }
-                    else
-                    {
-                        string rg = PickGame(roundCounter, r - 1, eventCounter, games, teamId, team2Id, numofRounds);
-                        roundPair += " " + rg.ToUpper() + " [" + teamNames[teamId] + "," + teamNames[team2Id] + "]";
-                    }
-                    
+
                 }
             }
+
+            //for (var r = 1; r < n; r++)
+            //{
+            //    roundPair += "\n" + r + ":";
+            //    for (var i = 1; i <= n / 2; i++)
+            //    {
+            //        if (i == 1)
+            //        {
+            //            teamId = 0;
+            //            team2Id = (n - 1 + r - 1) % (n - 1) + 1;
+            //            //roundPair += " [" + formatter(1) + "," + formatter((n - 1 + r - 1) % (n - 1) + 2) + "]";
+            //        }
+            //        else
+            //        {
+            //            teamId = (r + i - 2) % (n - 1) + 1;
+            //            team2Id = (n - 1 + r - i) % (n - 1) + 1;
+            //            //roundPair += " [" + formatter( (r+i-2) % (n-1) + 2) + "," + formatter((n - 1 + r - i) % (n - 1) + 2) + "]";
+            //        }
+            //        if (byeID >= 0 && (teamId == byeID || team2Id == byeID))
+            //        {
+            //            roundPair += " BYE  [" + teamNames[teamId] + "," + teamNames[team2Id] + "]";
+            //        }
+            //        else
+            //        {
+            //            string rg = PickGame(roundCounter, r - 1, eventCounter, games, teamId, team2Id, numofRounds);
+            //            roundPair += " " + rg.ToUpper() + " [" + teamNames[teamId] + "," + teamNames[team2Id] + "]";
+            //        }
+
+            //    }
+            //}
 
             return roundPair;
         }
@@ -130,7 +136,7 @@ namespace BackyardSchedules
             int numGames = listofGames.Count;
             int maxGames = numGames - 1;
             int maxLoops = numGames * rounds + 1; //prevent infinite loop below
-            
+
 
             //figure out the most even game to play
             int value1 = 0;
@@ -142,13 +148,13 @@ namespace BackyardSchedules
             {
                 if (loopCount > 0)
                 {
-                    if(value2 == maxGames)
+                    if (value2 == maxGames)
                     {
                         value1 = currentLow + 1;
                         value2 = value1;
                         currentLow++;
                     }
-                    else if (value1 ==  value2)
+                    else if (value1 == value2)
                     {
                         value1++;
                     }
@@ -194,20 +200,33 @@ namespace BackyardSchedules
 
         }
 
-        static string formatter(int x)
+        static int[,] GenerateGames(int numTeams)
         {
-            if (x < 10)
-                return "  " + x.ToString();
-            if (x < 100)
-                return " " + x.ToString();
-            else
-                return x.ToString();
+            int n2 = (int)((numTeams - 1) / 2);
+            int[,] results = new int[numTeams,numTeams];
+
+            int[] teams = new int[numTeams];
+            for (int i = 0; i < numTeams; i++) teams[i] = i;
+
+            for (int i = 0; i < numTeams; i++)
+            {
+                for (int ii = 0; ii < numTeams; ii++)
+                {
+                    if (i == ii || i > ii)
+                    {
+                        results.SetValue(-1, i, ii);
+                    }
+                }
+            }
+
+            return results;
         }
+
 
         static List<string> GenerateTeams(string teams)
         {
             char[] separators = { '\r', '\n' };
-            string[] teamArray =  teams.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            string[] teamArray = teams.Split(separators, StringSplitOptions.RemoveEmptyEntries);
             List<string> results = new List<string>();
 
             foreach (string t in teamArray)
@@ -216,232 +235,6 @@ namespace BackyardSchedules
             }
 
             return results;
-        }
-
-        // Return an array where results(i, j) gives
-        // the opponent of team i in round j.
-        // Note: num_teams must be odd.
-        static int[,] GenerateRoundRobinOdd(int num_teams)
-        {
-            int n2 = (int)((num_teams - 1) / 2);
-            int[,] results = new int[num_teams, num_teams];
-
-            // Initialize the list of teams.
-            int[] teams = new int[num_teams];
-            for (int i = 0; i < num_teams; i++) teams[i] = i;
-
-            // Start the rounds.
-            for (int round = 0; round < num_teams; round++)
-            {
-                for (int i = 0; i < n2; i++)
-                {
-                    int team1 = teams[n2 - i];
-                    int team2 = teams[n2 + i + 1];
-                    results[team1, round] = team2;
-                    results[team2, round] = team1;
-                }
-
-                // Set the team with the bye.
-                results[teams[0], round] = BYE;
-
-                // Rotate the array.
-                RotateArray(teams);
-            }
-
-            return results;
-        }
-
-        static int[,] GenerateRoundRobinEven(int num_teams)
-        {
-            // Generate the result for one fewer teams.
-            int[,] results = GenerateRoundRobinOdd(num_teams - 1);
-
-            // Copy the results into a bigger array,
-            // replacing the byes with the extra team.
-            int[,] results2 = new int[num_teams, num_teams - 1];
-            for (int team = 0; team < num_teams - 1; team++)
-            {
-                for (int round = 0; round < num_teams - 1; round++)
-                {
-                    if (results[team, round] == BYE)
-                    {
-                        // Change the bye to the new team.
-                        results2[team, round] = num_teams - 1;
-                        results2[num_teams - 1, round] = team;
-                    }
-                    else
-                    {
-                        results2[team, round] = results[team, round];
-                    }
-                }
-            }
-
-            return results2;
-        }
-
-        static int[,] GenerateRoundRobin(int num_teams)
-        {
-            if (num_teams % 2 == 0)
-                return GenerateRoundRobinEven(num_teams);
-            else
-                return GenerateRoundRobinOdd(num_teams);
-        }
-
-        // Rotate the entries one position.
-        static void RotateArray(int[] teams)
-        {
-            int tmp = teams[teams.Length - 1];
-            Array.Copy(teams, 0, teams, 1, teams.Length - 1);
-            teams[0] = tmp;
-        }
-
-
-        static string SetGame(int roundNum, int gameID, List<string> listofGames, string[] teamNames, int[,] matches, int[,] eventCounter, int[,] roundCounter, int maxLoops)
-        {
-            int numofTeams = teamNames.Length;
-            string result = "";
-
-            for (int team = 0; team < numofTeams; team++)
-            {
-                if (matches[team, roundNum] == BYE)
-                {
-                    //ignore for now
-                }
-                else if (roundCounter[roundNum,team] > 0)
-                {
-                    //ignore for now
-                }
-                //else if (team < matches[team, roundNum])
-                //{
-                    if (eventCounter[team, gameID] == 0 && eventCounter[matches[team, roundNum], gameID] == 0)
-                    {
-                        eventCounter[team, gameID]++;
-                        eventCounter[matches[team, roundNum], gameID]++;
-                        roundCounter[roundNum, team] = 1;
-                        roundCounter[roundNum, matches[team, roundNum]] = 1;
-                        result = listofGames[gameID].ToUpper() + " " + teamNames[team] + " vs " + teamNames[matches[team, roundNum]];
-                        break;
-                    }
-
-                //}
-            }
-
-            //now it gets tricky, someone has already played this game and it's time to repeat
-            int value1 = 0;
-            int value2 = 1;
-            int saveValue = 0;
-            int loops = 0;
-
-            while (result.Length == 0 && loops <= maxLoops)
-            {
-                if (loops > 0)
-                {
-                    value2++;
-                }
-
-                for (int team = 0; team < numofTeams; team++)
-                {
-                    if (matches[team, roundNum] == BYE)
-                    {
-                        //ignore for now
-                    }
-                    else if (roundCounter[roundNum, team] > 0)
-                    {
-                        //ignore for now
-                    }
-                    //else if (team < matches[team, roundNum])
-                    //{
-                        for (int attempt = 0; attempt < 2; attempt++)
-                        {
-                            if (attempt == 1 && value1 != value2)
-                            {
-                                saveValue = value2;
-                                value2 = value1;
-                                value1 = saveValue;
-                            }
-                            else if(attempt == 1)
-                            {
-                                team = numofTeams;
-                                break;
-                            }
-                            if (eventCounter[team, gameID] == value1 && eventCounter[matches[team, roundNum], gameID] == value2)
-                            {
-                                eventCounter[team, gameID]++;
-                                eventCounter[matches[team, roundNum], gameID]++;
-                                roundCounter[roundNum, team] = 1;
-                                roundCounter[roundNum, matches[team, roundNum]] = 1;
-                                result = listofGames[gameID].ToUpper() + " " + teamNames[team] + " vs " + teamNames[matches[team, roundNum]];
-                                team = numofTeams;
-                                break;
-                            }
-                       // }
-
-                    }
-
-                }
-                loops++;
-            }
-
-            //now it gets trickier, the games are becoming un-even.  
-            value1 = 0;
-            value2 = 2;
-            saveValue = 0;
-            loops = 0;
-            while (result.Length == 0 && loops <= maxLoops)
-            {
-                if (loops > 0)
-                {
-                    value1++;
-                    value2++;
-                }
-
-                for (int team = 0; team < numofTeams; team++)
-                {
-                    if (matches[team, roundNum] == BYE)
-                    {
-                        //ignore for now
-                    }
-                    else if (roundCounter[roundNum, team] > 0)
-                    {
-                        //ignore for now
-                    }
-                    else if (team < matches[team, roundNum])
-                    {
-                        for (int attempt = 0; attempt < 2; attempt++)
-                        {
-                            if (attempt == 1 && value1 != value2)
-                            {
-                                saveValue = value2;
-                                value2 = value1;
-                                value1 = saveValue;
-                            }
-                            else if (attempt == 1)
-                            {
-                                team = numofTeams;
-                                break;
-                            }
-                            if (eventCounter[team, gameID] == value1 && eventCounter[matches[team, roundNum], gameID] == value2)
-                            {
-                                eventCounter[team, gameID]++;
-                                eventCounter[matches[team, roundNum], gameID]++;
-                                roundCounter[roundNum, team] = 1;
-                                roundCounter[roundNum, matches[team, roundNum]] = 1;
-                                result = listofGames[gameID].ToUpper() + " " + teamNames[team] + " vs " + teamNames[matches[team, roundNum]];
-                                team = numofTeams;
-                                break;
-                            }
-                        }
-
-                    }
-
-                }
-                loops++;
-            }
-
-            if (result == "")
-                return "No Match Found";
-            else
-                return result;
         }
 
     }
