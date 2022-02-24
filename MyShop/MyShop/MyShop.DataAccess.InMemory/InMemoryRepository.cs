fe99dac7cb878b502829effaf443cc1e773529b1
robbie22811 <98604431+robbie22811@users.noreply.github.com>
2022-02-24 11:47:42 -0600

    Implemented Generic Repository

---
+++ MyShop/MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -0,0 +1,72 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class InMemoryRepository<T> where T : BaseEntity
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<T> items;
+        string className;
+
+        public InMemoryRepository()
+        {
+            className = typeof(T).Name;
+            items = cache[className] as List<T>;
+            if (items == null)
+            {
+                items = new List<T>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache[className] = items;
+        }
+
+        public void Insert(T t)
+        {
+            items.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            T toUpdate = items.Find(i => i.Id == t.Id);
+            if (toUpdate != null)
+                toUpdate = t;
+            else
+                throw new Exception(className + " Not Found");
+
+        }
+
+        public T Find(string id)
+        {
+            T t = items.Find(i => i.Id == id);
+            if (t != null)
+                return t;
+            else
+                throw new Exception(className + " Not Found");
+
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return items.AsQueryable();
+        }
+
+        public void Delete(string id)
+        {
+            T toDelete = items.Find(i => i.Id == id);
+            if (toDelete != null)
+                items.Remove(toDelete);
+            else
+                throw new Exception(className + " Not Found");
+
+        }
+    }
+}
