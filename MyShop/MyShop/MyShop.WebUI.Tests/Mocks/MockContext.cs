046cc2dd5e73388cf9fbb9fbaf99234fafdf63cb
robbie22811 <98604431+robbie22811@users.noreply.github.com>
2022-05-05 12:29:42 -0500

    tests

---
+++ MyShop/MyShop/MyShop.WebUI.Tests/Mocks/MockContext.cs
@@ -0,0 +1,67 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.WebUI.Tests.Mocks
+{
+    public class MockContext<T> : IRepository<T> where T : BaseEntity
+    {
+        List<T> items;
+        string className;
+
+        public MockContext()
+        {
+            items = new List<T>();
+        }
+
+        public void Commit()
+        {
+            return;
+        }
+
+        public void Insert(T t)
+        {
+            items.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            T toUpdate = items.Find(i => i.Id == t.Id);
+            if (toUpdate != null)
+                toUpdate = t;
+            else
+                throw new Exception(className + " Not Found");
+
+        }
+
+        public T Find(string id)
+        {
+            T t = items.Find(i => i.Id == id);
+            if (t != null)
+                return t;
+            else
+                throw new Exception(className + " Not Found");
+
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return items.AsQueryable();
+        }
+
+        public void Delete(string id)
+        {
+            T toDelete = items.Find(i => i.Id == id);
+            if (toDelete != null)
+                items.Remove(toDelete);
+            else
+                throw new Exception(className + " Not Found");
+
+        }
+
+    }
+}
